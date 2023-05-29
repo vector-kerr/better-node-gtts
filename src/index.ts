8a855570044369051a4c5e362693971361177acd
Daniel 'Vector' Kerr <vector@vector.id.au>
2023-05-30 08:11:57 +1000

    feat: add object-based constructor parameters

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,61 @@ import { LANGUAGES } from "./languages";
 
 const GOOGLE_TTS_URL = "http://translate.google.com/translate_tts";
 const DEFAULT_MAX_CHARS = 100;
+const DEFAULT_LANG = "en";
+
+interface Options {
+  lang?: string;
+  debug?: boolean;
+  maxChars?: number;
+  throwOnUnsupportedLanguage?: boolean;
+}
 
 export class Text2Speech {
   lang: string;
   debug: boolean;
   maxChars: number;
+  throwOnUnsupportedLanguage: boolean;
   getArgs: (text: string, index: number, total: number) => string;
 
-  constructor(_lang?: string, _debug?: boolean) {
-    this.lang = _lang ?? "en";
-    this.debug = _debug ?? false;
+  /**
+   * Create a new Text2Speech instance.
+   * This constructor offers two signatures - one for compatibility and one modern.
+   * @param {Options} opts Text2Speech options
+   * @param {string} opts.lang The two-letter code for the language to use (default=en)
+   * @param {boolean} opts.debug Enable debugging (default=false)
+   * @param {number} opts.maxChars The maximum number of characters supported in a single message (default=100)
+   * @param {boolean} opts.throwOnUnsupportedLanguage Throw an exception if the supplied language is unsupported (default=true)
+   * @param {string} lang The two-letter code for the language to use (default=en)
+   * @param {boolean} debug Enable debugging (default=false)
+   *
+   * @example
+   * const tts = new Text2Speech({lang: 'en', debug: false, maxChars: 50})
+   * 
+   * @example
+   * const tts = new Text2Speech("en", false)
+   */
+  constructor() {
+    if (typeof arguments[0] === "object") {
+      const opts: Options = arguments[0] as Options;
+      this.lang = opts.lang ?? DEFAULT_LANG;
+      this.debug = opts.debug ?? false;
+      this.maxChars = opts.maxChars ?? DEFAULT_MAX_CHARS;
+      this.throwOnUnsupportedLanguage = opts.throwOnUnsupportedLanguage ?? true;
+    } else {
+      this.lang = arguments[0] ?? DEFAULT_LANG;
+      this.debug = arguments[1] ?? false;
+      this.maxChars = DEFAULT_MAX_CHARS;
+    }
+
     this.lang = this.lang.toLowerCase();
-    this.maxChars = DEFAULT_MAX_CHARS;
     this.getArgs = this.getArgsFactory(this.lang);
 
     if (LANGUAGES[this.lang] === undefined) {
-      throw new Error("Language not supported: " + this.lang);
+      if (this.throwOnUnsupportedLanguage) {
+        throw new Error("Language not supported: " + this.lang);
+      } else {
+        console.warn("Language not supported: " + this.lang);
+      }
     }
   }
 
@@ -58,33 +97,35 @@ export class Text2Speech {
     const textParts = this.tokenize(text);
     const total = textParts.length;
 
-    const streams = await Promise.all(textParts.map((part, index) => {
-      return new Promise((resolve) => {
-        const headers = this.getHeader();
-        const args = this.getArgs(part, index, total);
-        const fullUrl = GOOGLE_TTS_URL + args;
+    const streams = (await Promise.all(
+      textParts.map((part, index) => {
+        return new Promise((resolve) => {
+          const headers = this.getHeader();
+          const args = this.getArgs(part, index, total);
+          const fullUrl = GOOGLE_TTS_URL + args;
 
-        axios({
-          url: fullUrl,
-          headers,
-          method: "GET",
-          responseType: "stream",
-        }).then((response) => {
-          resolve(response.data);
+          axios({
+            url: fullUrl,
+            headers,
+            method: "GET",
+            responseType: "stream",
+          }).then((response) => {
+            resolve(response.data);
+          });
         });
-      });
-    })) as LazyStream[];
+      })
+    )) as LazyStream[];
 
     return new MultiStream(streams);
   }
 
   getHeader() {
     const headers = {
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/536.26.17 (KHTML like Gecko) Version/6.0.2 Safari/536.26.17",
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/536.26.17 (KHTML like Gecko) Version/6.0.2 Safari/536.26.17",
     };
 
     if (this.debug) console.log(headers);
-
     return headers;
   }
 
@@ -143,11 +184,12 @@ export class Text2Speech {
       } else {
         console.log(req.headers);
         res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({
-          code: -1,
-          message:
-            `Missing text. Please try: ${req.headers.host}?text=your+text`,
-        }));
+        res.end(
+          JSON.stringify({
+            code: -1,
+            message: `Missing text. Please try: ${req.headers.host}?text=your+text`,
+          })
+        );
       }
     });
 
